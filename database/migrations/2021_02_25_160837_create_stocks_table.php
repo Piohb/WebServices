e7d3837a5eee98741d3bbec503b85840337e1892
anguinea <58850607+anguinea@users.noreply.github.com>
2021-02-25 17:21:31 +0100

    construction migration

---
+++ database/migrations/2021_02_25_160837_create_stocks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('product_id');
+            $table->foreignId('shop_id');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->integer('sales')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+}
