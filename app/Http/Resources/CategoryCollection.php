5eba1fbc218e6a4eed9262a185b657dd4dcd1a86
emeric <emeric.l@apps-panel.com>
2021-02-26 10:07:28 +0100

    Collections

--- app/Http/Resources/CategoryCollection.php
+++ app/Http/Resources/CategoryCollection.php
@@ -3,13 +3,13 @@
  * Created by PhpStorm.
  * User: Emeric
  * Date: 26/02/2021
- * Time: 09:38
+ * Time: 09:43
  */
 
-namespace App\Http\Controllers;
+namespace App\Http\Resources;
 
 
-class Controller
+class CategoryCollection
 {
 
 }
--- app/Http/Resources/StockCollection.php
+++ app/Http/Resources/StockCollection.php
@@ -3,13 +3,13 @@
  * Created by PhpStorm.
  * User: Emeric
  * Date: 26/02/2021
- * Time: 09:38
+ * Time: 09:48
  */
 
-namespace App\Http\Controllers;
+namespace App\Http\Resources;
 
 
-class Controller
+class StockCollection
 {
 
 }
